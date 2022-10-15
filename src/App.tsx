d4b9c494f19c8bb824a5955aca45609779d1eea3
Leandro Pedroso <lpedroso@meudroz.com>
2022-10-15 14:47:45 -0300

    Atualizando estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Heading } from "./components/Heading";
-import { Text } from "./components/Text";
-import { TextInput } from "./components/TextInput";
 import { Logo } from "./Logo";
 import { Envelope, Lock } from "phosphor-react";
 import "./styles/global.css";
-import { Checkbox } from "./components/Checkbox";
-import { Button } from "./components/Button";
+
+import { Button, Checkbox, TextInput, Text, Heading } from "./components";
 
 export function App() {
   return (
---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+// Button
+export { Button } from "./Button/Button";
+export type { ButtonProps } from "./Button/Button";
+
+// Checkbox
+export { Checkbox } from "./Checkbox/Checkbox";
+export type { CheckboxProps } from "./Checkbox/Checkbox";
+
+// Heading
+export { Heading } from "./Heading/Heading";
+export type { HeadingProps } from "./Heading/Heading";
+
+// Inputs
+// TextInput
+export { TextInput } from "./Inputs/TextInput/TextInput";
+export type {
+  TextInputIconProps,
+  TextInputInputProps,
+  TextInputRootProps,
+} from "./Inputs/TextInput/TextInput";
+
+// Text
+export { Text } from "./Text/Text";
+export type { TextProps } from "./Text/Text";
